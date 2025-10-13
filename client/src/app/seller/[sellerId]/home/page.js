df02fd959169416aa803bc899b830ff2f6805ec6
aaditya paudel <adityaspaudel@gmail.com>
2025-10-13 15:09:11 +0545

    latest update

--- client/src/app/seller/[sellerId]/home/page.js
+++ client/src/app/seller/[sellerId]/home/page.js
@@ -1,12 +1,39 @@
 'use client';
 
-import ProductUploadPage from "@/components/productUploadPage";
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import AddProducts from '@/components/addProducts';
+import { useParams } from 'next/navigation';
+import axios from 'axios';
 
 const SellerHome = () => {
+  const { sellerId } = useParams();
+  // console.log(sellerId);
+  const [products, setProducts] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get(
+          `http://localhost:8000/seller/${sellerId}/getProducts`
+        );
+        console.log('data: ', data);
+
+        setProducts(data);
+      } catch (error) {
+      } finally {
+        setProducts('');
+      }
+    };
+
+    if (sellerId) {
+      fetchProducts();
+    }
+  });
+
   return (
-    <div>
-      <ProductUploadPage />
+    <div className="bg-amber-50 text-black">
+      <AddProducts sellerId={sellerId} />
+      {JSON.stringify(products, 2, 2)}{' '}
     </div>
   );
 };
