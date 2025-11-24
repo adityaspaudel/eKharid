989efd9cc4bf33820baaf86dde50d2426a80c805
aaditya paudel <adityaspaudel@gmail.com>
2025-11-24 22:32:11 +0545

    database update

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
+// MONGODB_ATLAS_URI="mongodb+srv://adityaspaudel:adityaspaudel@cluster0.65gve6j.mongodb.net/?appName=Cluster0"
 const dbConnect = async () => {
   try {
     const isConnected = await mongoose.connect(process.env.MONGODB_ATLAS_URI);
